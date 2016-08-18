ef86bc72b32178719cfb6b7f037f7449ab824c9f
Kevin Escobar Miranda <info@EscobarMiranda.com>
2016-08-18 11:59:37 -0600

    first commit

---
+++ www/app/login/login.controller.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.login')
+    .controller('LoginCtrl', LoginCtrl);
+
+  /* @ngInject */
+  function LoginCtrl() {
+
+    var vm = this;
+
+    activate();
+
+    function activate() {
+      
+    }
+
+  }
+})();
