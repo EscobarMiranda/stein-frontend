abb43f732fb2b008a78877b44f9769b03c9eac8f
Kevin Escobar Miranda <info@EscobarMiranda.com>
2016-08-19 10:32:45 -0600

    Create menu, contact list, contact information, comments and locations views

---
+++ www/app/contact/contact.controller.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.contact')
+    .controller('ContactCtrl', ContactCtrl);
+
+  /* @ngInject */
+  function ContactCtrl() {
+
+    var vm = this;
+
+    activate();
+
+    function activate() {
+      
+    }
+
+  }
+})();
