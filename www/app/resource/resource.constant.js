80f8c30c80d54752d0614504a72ca842c351aa09
Kevin Escobar Miranda <info@EscobarMiranda.com>
2016-11-09 15:10:57 -0600

    Add heroku config

--- www/app/resource/resource.constant.js
+++ www/app/resource/resource.constant.js
@@ -4,7 +4,7 @@
   angular
     .module('app.resource')
     .constant('RESOURCE', {
-      'API_URL': 'http://localhost:55411/api/'
+      'API_URL': 'http://stein-backend.azurewebsites.net/api/'
     });
 
 })();
